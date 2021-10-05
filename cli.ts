81b98f7cb8844d32cfd7f729de8e1145ec0f538e
David Sherret <dsherret@gmail.com>
2021-10-05 10:46:38 -0400

    feat: improve help text to mention compiler option flags
    Also, move everything into _args.ts.

--- cli.ts
+++ cli.ts
@@ -1,23 +1,15 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 
-import { parse } from "https://deno.land/std@0.109.0/flags/mod.ts";
-import { parseArgs } from "./lib/_args.ts";
+import { outputUsage, parseArgs } from "./lib/_args.ts";
 import { outputDiagnostics } from "./lib/_compiler.ts";
 import { emit } from "./mod.ts";
 
-const cliArgs = parse(Deno.args, {
-  alias: {
-    h: "help",
-  },
-});
+const args = parseArgs(Deno.args);
 
-if (cliArgs.help) {
-  usage();
+if (args === "help") {
+  outputUsage();
   Deno.exit(0);
-}
-
-const args = parseArgs(cliArgs);
-if (args instanceof Array) {
+} else if (args instanceof Array) {
   outputDiagnostics(args);
   Deno.exit(1);
 }
@@ -32,21 +24,4 @@ const emitResult = await emit({
 if (!emitResult.success) {
   outputDiagnostics(emitResult.diagnostics);
   Deno.exit(1);
-}
-
-function usage() {
-  console.log(`Usage: dnt <entrypoint> [options]
-
-Options:
-  -h, --help                  Shows the help message.
-  --target <target>           Specifies the transpile target eg. ES6, ESNext, etc
-  --outDir <dir>              The output directory.
-  --declaration               Outputs the declaration files.
-  --shimPackageName <name>    Specifies the shim package name for 'Deno' namespace.
-  --typeCheck                 Performs type checking.
-
-Examples:
-  # Outputs to ./npm/dist
-  dnt mod.ts --target ES6 --outDir ./npm/dist --declaration
-`);
 }
--- lib/_args.ts
+++ lib/_args.ts
@@ -1,6 +1,6 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 
-import { Args } from "https://deno.land/std@0.109.0/flags/mod.ts";
+import { parse } from "https://deno.land/std@0.109.0/flags/mod.ts";
 import { ts } from "./_mod.deps.ts";
 
 export interface ParsedArgs {
@@ -10,7 +10,19 @@ export interface ParsedArgs {
   typeCheck: boolean;
 }
 
-export function parseArgs(cliArgs: Args): ParsedArgs | ts.Diagnostic[] {
+export function parseArgs(
+  args: string[],
+): ParsedArgs | ts.Diagnostic[] | "help" {
+  const cliArgs = parse(args, {
+    alias: {
+      h: "help",
+    },
+  });
+
+  if (cliArgs.help || args.length === 0) {
+    return "help";
+  }
+
   const entryPoint = takeEntryPoint();
   const typeCheck = takeTypeCheck();
   const shimPackageName = takeShimPackageName();
@@ -65,4 +77,30 @@ export function parseArgs(cliArgs: Args): ParsedArgs | ts.Diagnostic[] {
     }
     return args;
   }
+}
+
+export function outputUsage() {
+  console.log(`Usage: dnt <entrypoint> [options]
+
+Options:
+  -h, --help                  Shows the help message.
+  --shimPackageName <name>    Specifies the shim package name for 'Deno' namespace.
+  --typeCheck                 Performs type checking.
+
+Compiler options:
+
+  dnt supports the same compiler options that tsc supports.
+
+    https://www.typescriptlang.org/docs/handbook/compiler-options.html
+
+  For example, a small selection:
+
+  --target <target>           Specifies the transpile target eg. ES6, ESNext, etc
+  --outDir <dir>              The output directory (required)
+  --declaration               Outputs the declaration files.
+
+Examples:
+  # Outputs to ./npm/dist
+  dnt mod.ts --target ES6 --outDir ./npm/dist --declaration --module commonjs
+`);
 }
