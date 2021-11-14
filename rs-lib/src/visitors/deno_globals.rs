173e6e1b347e7629c82f373fec47de729f3012c0
David Sherret <dsherret@gmail.com>
2021-11-14 15:31:45 -0500

    Cargo fmt.

--- rs-lib/src/visitors/deno_globals.rs
+++ rs-lib/src/visitors/deno_globals.rs
@@ -12,8 +12,20 @@ use deno_ast::view::*;
 use crate::text_changes::TextChange;
 
 const DENO_SHIM_GLOBAL_NAMES: [&str; 14] = [
-  "Blob", "crypto", "Deno", "fetch", "File", "FormData", "Headers", "Request",
-  "Response", "alert", "confirm", "prompt", "setTimeout", "setInterval",
+  "Blob",
+  "crypto",
+  "Deno",
+  "fetch",
+  "File",
+  "FormData",
+  "Headers",
+  "Request",
+  "Response",
+  "alert",
+  "confirm",
+  "prompt",
+  "setTimeout",
+  "setInterval",
 ];
 
 pub struct GetDenoGlobalTextChangesParams<'a> {
