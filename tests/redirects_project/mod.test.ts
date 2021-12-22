0c89ea9474fd1b3964c0c7b2aa1519d0465e7892
David Sherret <dsherret@gmail.com>
2021-12-22 17:31:36 -0500

    Update which_runtime

--- tests/redirects_project/mod.test.ts
+++ tests/redirects_project/mod.test.ts
@@ -1,6 +1,6 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 
-import { isDeno } from "https://deno.land/x/which_runtime@0.1.0/mod.ts";
+import { isDeno } from "https://deno.land/x/which_runtime@0.2.0/mod.ts";
 import { output } from "./mod.ts";
 
 Deno.test("should add in test project", () => {
