67b47f9b2a6859f2c55614f06968e283ee36ed41
David Sherret <dsherret@gmail.com>
2022-02-05 14:21:00 -0500

    feat: add emptyDir re-export and recommend using it

--- lib/mod.deps.ts
+++ lib/mod.deps.ts
@@ -8,3 +8,4 @@ export {
 export { default as CodeBlockWriter } from "https://deno.land/x/code_block_writer@11.0.0/mod.ts";
 export * as colors from "https://deno.land/std@0.119.0/fmt/colors.ts";
 export * as glob from "https://deno.land/std@0.119.0/fs/expand_glob.ts";
+export { emptyDir } from "https://deno.land/std@0.119.0/fs/empty_dir.ts";
