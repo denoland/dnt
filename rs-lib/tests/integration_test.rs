0b98451b792d9ec80719c35bccf239bee80a36e8
Yongwook Choi <yongwook.choi@riiid.co>
2022-03-04 20:54:55 +0900

    format

--- rs-lib/tests/integration_test.rs
+++ rs-lib/tests/integration_test.rs
@@ -301,32 +301,45 @@ async fn transform_shim_custom_shims() {
 async fn transform_shim_node_custom_shims() {
   let result = TestBuilder::new()
     .with_loader(|loader| {
-      loader.add_local_file("/mod.ts", "const readableStream = new ReadableStream();");
+      loader.add_local_file(
+        "/mod.ts",
+        "const readableStream = new ReadableStream();",
+      );
     })
     .add_shim(Shim::Module(ModuleShim {
       module: "node:stream/web".to_string(),
       global_names: vec![GlobalName {
         name: "ReadableStream".to_string(),
         export_name: None,
-        type_only: false
-      }]
-    })).transform().await.unwrap();
+        type_only: false,
+      }],
+    }))
+    .transform()
+    .await
+    .unwrap();
 
-  assert_files!(result.main.files, &[(
-    "_dnt.shims.ts",
-    get_shim_file_text(
-      concat!(
-        "import { ReadableStream } from \"node:stream/web\";\n",
-        "export { ReadableStream } from \"node:stream/web\";\n",
-        "\n",
-        "const dntGlobals = {\n",
-        "  ReadableStream,\n",
-        "};\n",
-        "export const dntGlobalThis = createMergeProxy(globalThis, dntGlobals);\n",
-      ).to_string(),
+  assert_files!(result.main.files, &[
+    (
+      "_dnt.shims.ts",
+      get_shim_file_text(
+        concat!(
+          "import { ReadableStream } from \"node:stream/web\";\n",
+          "export { ReadableStream } from \"node:stream/web\";\n",
+          "\n",
+          "const dntGlobals = {\n",
+          "  ReadableStream,\n",
+          "};\n",
+          "export const dntGlobalThis = createMergeProxy(globalThis, dntGlobals);\n",
+        ).to_string(),
+      ),
     ),
-  ),
-  ("mod.ts", concat!("import * as dntShim from \"./_dnt.shims.js\";\n","const readableStream = new dntShim.ReadableStream();").to_string())
+    (
+      "mod.ts",
+      concat!(
+        "import * as dntShim from \"./_dnt.shims.js\";\n",
+        "const readableStream = new dntShim.ReadableStream();"
+      ).to_string()
+    )
   ]);
 }
 
