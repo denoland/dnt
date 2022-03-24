66413972e61c74b82f62646d7188e5445690c99b
David Sherret <dsherret@gmail.com>
2022-03-24 13:24:44 -0400

    Format.

--- rs-lib/src/lib.rs
+++ rs-lib/src/lib.rs
@@ -408,10 +408,7 @@ pub async fn transform(options: TransformOptions) -> Result<TransformOutput> {
       }
       ModuleKind::Asserted => {
         if let Some(source) = &module.maybe_source {
-          format!(
-            "export default {};",
-            strip_bom(source).trim(),
-          )
+          format!("export default {};", strip_bom(source).trim(),)
         } else {
           continue;
         }
