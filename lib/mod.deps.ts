17a52d4710a2df1acaf759b1d74f9f0639bea27a
David Sherret <dsherret@gmail.com>
2022-06-15 14:04:41 -0400

    chore: upgrade deno_std to 0.143

--- lib/mod.deps.ts
+++ lib/mod.deps.ts
@@ -6,6 +6,6 @@ export {
   ts,
 } from "https://deno.land/x/ts_morph@15.1.0/bootstrap/mod.ts";
 export { default as CodeBlockWriter } from "https://deno.land/x/code_block_writer@11.0.0/mod.ts";
-export * as colors from "https://deno.land/std@0.140.0/fmt/colors.ts";
-export * as glob from "https://deno.land/std@0.140.0/fs/expand_glob.ts";
-export { emptyDir } from "https://deno.land/std@0.140.0/fs/empty_dir.ts";
+export * as colors from "https://deno.land/std@0.143.0/fmt/colors.ts";
+export * as glob from "https://deno.land/std@0.143.0/fs/expand_glob.ts";
+export { emptyDir } from "https://deno.land/std@0.143.0/fs/empty_dir.ts";
--- tests/polyfill_project/mod.test.ts
+++ tests/polyfill_project/mod.test.ts
@@ -1,7 +1,7 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 
 import { hasOwn } from "./mod.ts";
-import { assertEquals } from "https://deno.land/std@0.140.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.143.0/testing/asserts.ts";
 
 Deno.test("should test the polyfill", () => {
   assertEquals(hasOwn({}), false);
