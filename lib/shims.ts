52e313ce0e6fb980964fa5abd34c1116523175ab
David Sherret <dsherret@users.noreply.github.com>
2022-07-21 12:45:59 -0400

    feat: upgrade for deno 1.24 (#197)

--- lib/shims.ts
+++ lib/shims.ts
@@ -103,7 +103,7 @@ function getDenoShim(): Shim {
   return {
     package: {
       name: "@deno/shim-deno",
-      version: "~0.7.0",
+      version: "~0.8.0",
     },
     globalNames: ["Deno"],
   };
@@ -197,7 +197,7 @@ function getUndiciShim(): Shim {
   return {
     package: {
       name: "undici",
-      version: "^5.5.1",
+      version: "^5.8.0",
     },
     globalNames: [
       "fetch",
