c074d08e780ce5356cee9da87a2a8b2657189e9f
David Sherret <dsherret@gmail.com>
2022-09-26 15:57:30 -0400

    Format and remove other compiler option since it's always true in deno code.

--- mod.ts
+++ mod.ts
@@ -237,10 +237,10 @@ export async function build(options: BuildOptions): Promise<void> {
       esModuleInterop: false,
       isolatedModules: options.compilerOptions?.isolatedModules ?? true,
       useDefineForClassFields: true,
-      experimentalDecorators:
-        options.compilerOptions?.experimentalDecorators ?? true,
-      emitDecoratorMetadata:
-        options.compilerOptions?.emitDecoratorMetadata ?? false,
+      experimentalDecorators: options.compilerOptions?.experimentalDecorators ??
+        true,
+      emitDecoratorMetadata: options.compilerOptions?.emitDecoratorMetadata ??
+        false,
       jsx: ts.JsxEmit.React,
       jsxFactory: "React.createElement",
       jsxFragmentFactory: "React.Fragment",
@@ -249,7 +249,7 @@ export async function build(options: BuildOptions): Promise<void> {
       moduleResolution: ts.ModuleResolutionKind.NodeJs,
       target: compilerScriptTarget,
       lib: libNamesToCompilerOption(
-        options.compilerOptions?.lib ?? getCompilerLibOption(scriptTarget)
+        options.compilerOptions?.lib ?? getCompilerLibOption(scriptTarget),
       ),
       allowSyntheticDefaultImports: true,
       importHelpers: options.compilerOptions?.importHelpers,
@@ -266,17 +266,19 @@ export async function build(options: BuildOptions): Promise<void> {
         path: transformOutput.main.entryPoints[i],
       }))
       .filter((p) => p.kind === "bin")
-      .map((p) => p.path)
+      .map((p) => p.path),
   );
 
-  for (const outputFile of [
-    ...transformOutput.main.files,
-    ...transformOutput.test.files,
-  ]) {
+  for (
+    const outputFile of [
+      ...transformOutput.main.files,
+      ...transformOutput.test.files,
+    ]
+  ) {
     const outputFilePath = path.join(
       options.outDir,
       "src",
-      outputFile.filePath
+      outputFile.filePath,
     );
     const outputFileText = binaryEntryPointPaths.has(outputFile.filePath)
       ? `#!/usr/bin/env node\n${outputFile.fileText}`
@@ -292,10 +294,10 @@ export async function build(options: BuildOptions): Promise<void> {
             `(See ${outputFile.filePath} ${tlaLocation.line + 1}:${
               tlaLocation.character + 1
             }). ` +
-            `Please re-organize your code to not use a top level await or only distribute an ES module by setting the 'scriptModule' build option to false.`
+            `Please re-organize your code to not use a top level await or only distribute an ES module by setting the 'scriptModule' build option to false.`,
         );
         throw new Error(
-          "Build failed due to top level await when creating CommonJS/UMD package."
+          "Build failed due to top level await when creating CommonJS/UMD package.",
         );
       }
     }
@@ -344,7 +346,7 @@ export async function build(options: BuildOptions): Promise<void> {
     emit();
     writeFile(
       path.join(esmOutDir, "package.json"),
-      `{\n  "type": "module"\n}\n`
+      `{\n  "type": "module"\n}\n`,
     );
   }
 
@@ -355,10 +357,9 @@ export async function build(options: BuildOptions): Promise<void> {
       declaration: false,
       esModuleInterop: true,
       outDir: scriptOutDir,
-      module:
-        options.scriptModule === "umd"
-          ? ts.ModuleKind.UMD
-          : ts.ModuleKind.CommonJS,
+      module: options.scriptModule === "umd"
+        ? ts.ModuleKind.UMD
+        : ts.ModuleKind.CommonJS,
     });
     program = project.createProgram();
     emit({
@@ -368,7 +369,7 @@ export async function build(options: BuildOptions): Promise<void> {
     });
     writeFile(
       path.join(scriptOutDir, "package.json"),
-      `{\n  "type": "commonjs"\n}\n`
+      `{\n  "type": "commonjs"\n}\n`,
     );
   }
 
@@ -407,7 +408,7 @@ export async function build(options: BuildOptions): Promise<void> {
       },
       undefined,
       opts?.onlyDtsFiles,
-      opts?.transformers
+      opts?.transformers,
     );
 
     if (emitResult.diagnostics.length > 0) {
@@ -430,7 +431,7 @@ export async function build(options: BuildOptions): Promise<void> {
     });
     writeFile(
       path.join(options.outDir, "package.json"),
-      JSON.stringify(packageJsonObj, undefined, 2)
+      JSON.stringify(packageJsonObj, undefined, 2),
     );
   }
 
@@ -451,10 +452,10 @@ export async function build(options: BuildOptions): Promise<void> {
       entryPoints: entryPoints.map((e) => e.path),
       testEntryPoints: options.test
         ? await glob({
-            pattern: getTestPattern(),
-            rootDir: options.rootTestDir ?? Deno.cwd(),
-            excludeDirs: [options.outDir],
-          })
+          pattern: getTestPattern(),
+          rootDir: options.rootTestDir ?? Deno.cwd(),
+          excludeDirs: [options.outDir],
+        })
         : [],
       shims,
       testShims,
@@ -473,31 +474,29 @@ export async function build(options: BuildOptions): Promise<void> {
   }
 
   function createTestLauncherScript() {
-    const denoTestShimPackage =
-      getDependencyByName("@deno/shim-deno-test") ??
+    const denoTestShimPackage = getDependencyByName("@deno/shim-deno-test") ??
       getDependencyByName("@deno/shim-deno");
     writeFile(
       path.join(options.outDir, "test_runner.js"),
       transformCodeToTarget(
         getTestRunnerCode({
-          denoTestShimPackageName:
-            denoTestShimPackage == null
-              ? undefined
-              : denoTestShimPackage.name === "@deno/shim-deno"
-              ? "@deno/shim-deno/test-internals"
-              : denoTestShimPackage.name,
+          denoTestShimPackageName: denoTestShimPackage == null
+            ? undefined
+            : denoTestShimPackage.name === "@deno/shim-deno"
+            ? "@deno/shim-deno/test-internals"
+            : denoTestShimPackage.name,
           testEntryPoints: transformOutput.test.entryPoints,
           includeEsModule: options.esModule !== false,
           includeScriptModule: options.scriptModule !== false,
         }),
-        compilerScriptTarget
-      )
+        compilerScriptTarget,
+      ),
     );
 
     function getDependencyByName(name: string) {
       return (
         transformOutput.test.dependencies.find((d) => d.name === name) ??
-        transformOutput.main.dependencies.find((d) => d.name === name)
+          transformOutput.main.dependencies.find((d) => d.name === name)
       );
     }
   }
@@ -508,7 +507,7 @@ export async function build(options: BuildOptions): Promise<void> {
     // * or ending with `_test.{ts, tsx, js, mjs, jsx}`
     return (
       options.testPattern ??
-      "**/{test.{ts,tsx,js,mjs,jsx},*.test.{ts,tsx,js,mjs,jsx},*_test.{ts,tsx,js,mjs,jsx}}"
+        "**/{test.{ts,tsx,js,mjs,jsx},*.test.{ts,tsx,js,mjs,jsx},*_test.{ts,tsx,js,mjs,jsx}}"
     );
   }
 }
