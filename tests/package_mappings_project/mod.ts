006a45ac464c64f76facc0c3f3b86107d4756ac9
David Sherret <dsherret@gmail.com>
2022-12-19 00:18:24 -0500

    chore: fix broken test in latest deno

--- tests/package_mappings_project/mod.ts
+++ tests/package_mappings_project/mod.ts
@@ -1,5 +1,6 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 import CodeBlockWriter from "https://deno.land/x/code_block_writer@11.0.0/mod.ts";
+// @deno-types="./using-statement-temp.d.ts"
 import { using } from "npm:using-statement@^0.4";
 
 export function getResult() {
---
+++ tests/package_mappings_project/using-statement-temp.d.ts
@@ -0,0 +1,2 @@
+// temp file... remove this later after updating using_statement
+export const using: any;
