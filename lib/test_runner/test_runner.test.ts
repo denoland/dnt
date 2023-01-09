d43f81d443e14d3137924fb7e6a7647048f76e7d
David Sherret <dsherret@gmail.com>
2023-01-09 17:29:24 -0500

    chore: bump copyright

--- lib/test_runner/test_runner.test.ts
+++ lib/test_runner/test_runner.test.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
 import { runTestDefinitions } from "./test_runner.ts";
 import { assertEquals, assertRejects } from "../test.deps.ts";
--- tests/test_project/mod.ts
+++ tests/test_project/mod.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
 export function add(a: number, b: number) {
   return a + b;
