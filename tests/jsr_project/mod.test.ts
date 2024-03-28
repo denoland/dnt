07ccc725bab65b21f187c08dfce0abd9d6c743e0
David Sherret <dsherret@users.noreply.github.com>
2024-03-27 20:32:40 -0400

    chore: pin `@std` dep (#397)

--- tests/jsr_project/mod.test.ts
+++ tests/jsr_project/mod.test.ts
@@ -1,7 +1,7 @@
 // Copyright 2018-2024 the Deno authors. MIT license.
 
 import { add } from "./mod.ts";
-import { assertEquals } from "jsr:@std/assert@0.220/assert_equals";
+import { assertEquals } from "jsr:@std/assert@0.221/assert-equals";
 
 Deno.test("should add in test project", () => {
   assertEquals(add(1, 2), 3);
