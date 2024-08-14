df0166427213d8393d56443501f29684c12e3419
Joscha Feth <joscha@feth.com>
2024-08-14 10:34:44 +0100

    fix: make scripts always present with empty default

--- lib/package_json.ts
+++ lib/package_json.ts
@@ -94,7 +94,7 @@ export function getPackageJson({
       // override with specified scripts
       ...(packageJsonObj.scripts ?? {}),
     })
-    : packageJsonObj.scripts;
+    : packageJsonObj.scripts ?? {};
   const mainExport = exports.length > 0
     ? {
       module: includeEsModule ? `./esm/${exports[0].path}` : undefined,
